2b8ea2df90bc7182b04ca66baa7bc6343ec1e780
Andreas Hallberg <anhallbe@gmail.com>
2019-11-15 01:08:33 +0100

    background center

--- src/app/background.directive.ts
+++ src/app/background.directive.ts
@@ -7,6 +7,7 @@ export class BackgroundDirective {
 
   @Input('appBackground') url: string;
   @HostBinding('style.background-size') backgroundSize = 'cover';
+  @HostBinding('style.background-position') backgroundPosition = 'center';
   @HostBinding('style.background-image') get background() {
     return `url(${this.url})`;
   }
